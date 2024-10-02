cb22ffcc8ab57d3f6c1833c524dfb9f0fa1d8404
obay.developer <obay.developer@gmail.com>
2024-10-02 09:47:08 +0000

    update content

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
  
-url = "https://www.youtube.com/watch?v=vQJez9iiS7Y"
+url = "https://www.youtube.com/watch?v=gIr9TQnV25A"
  
 yt = YouTube(url, on_progress_callback = on_progress)
 print(yt.title)
